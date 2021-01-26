8865c9acf0d81b58f0ebe0fca297f772323d29a3
augustodelg <adg.prodlp@gmail.com>
2021-01-26 14:39:33 -0300

    custom note view

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
     has_many :notes, dependent: :delete_all
     belongs_to :user
+
+    validates :name, presence: true
+    validates_uniqueness_of :name, scope: :user_id
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ApplicationRecord
     belongs_to :book
     has_rich_text :content 
+    validates :title, presence: true
+    validates_uniqueness_of :title, scope: :book_id
 end
