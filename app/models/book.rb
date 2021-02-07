17dca34c2ad9d1bfb7af1575b7d341a05765e606
augustodelg <adg.prodlp@gmail.com>
2021-02-07 16:51:47 -0300

    Delete before destroy of book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,7 @@
 class Book < ApplicationRecord
-    before_destroy :delete_content
     has_many :notes, dependent: :destroy
     belongs_to :user
     validates :name, presence: true, length: { maximum: 150, too_long: "%{count} characters is the maximum allowed" }
     validates_uniqueness_of :name, scope: :user_id
-    
-    def delete_content
-        p "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBbb"
-    end
+
 end
