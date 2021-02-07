a0db1c41f2e9a6444e08adbdaeb478236639fc80
augustodelg <adg.prodlp@gmail.com>
2021-02-07 14:05:18 -0300

    Fix destroy methods

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,18 +1,11 @@
 class Book < ApplicationRecord
-    after_destroy :stop_destroy
-
-    has_many :notes, dependent: :delete_all
+    before_destroy :delete_content
+    has_many :notes, dependent: :destroy
     belongs_to :user
-
-    validates :name, presence: true, length: { maximum: 32, too_long: "%{count} characters is the maximum allowed" }
+    validates :name, presence: true, length: { maximum: 150, too_long: "%{count} characters is the maximum allowed" }
     validates_uniqueness_of :name, scope: :user_id
-
-    def stop_destroy
-        if name == 'Global'
-            errors.add(:base, "You dont delete the Global book")
-            p 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-        end
-        
-    end
     
+    def delete_content
+        p "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBbb"
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   after_create :create_global_book
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :books, dependent: :delete_all
+  has_many :books, dependent: :destroy
   has_many :notes, through: :books
 
   def create_global_book
     books.create(:name => 'Global')
   end
+  
 end
